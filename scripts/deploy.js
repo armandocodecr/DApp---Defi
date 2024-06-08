1d431d2bf171aa20631767ee1ccf2ae1454d3d3b
ArmandoCode <totovip1464@gmail.com>
2024-06-07 20:41:20 -0600

    update: save the contract address

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,12 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+// import { ethers } from 'hardhat';
+
+// Obtener la ruta del directorio actual
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 async function main() {
   const [deployer] = await ethers.getSigners();
 
@@ -24,12 +33,24 @@ async function main() {
 
   // Transferir JamTokens a la cuenta de MetaMask
   const metamaskAddress = '0xa14C5017c5b3d5b03A96d94e07A3B200D6085085'; // Reemplaza con la dirección de tu cuenta de MetaMask
-  await jamToken.transfer(metamaskAddress, ethers.utils.parseUnits('100', 18));
+  await jamToken.transfer(metamaskAddress, ethers.utils.parseUnits('10000', 18));
   console.log(`Transferred 100 JamTokens to account: ${metamaskAddress}`);
 
   // Verificar balance
   const balance = await jamToken.balanceOf(metamaskAddress);
   console.log(`Balance of ${metamaskAddress}: ${ethers.utils.formatUnits(balance, 18)} JAM`);
+
+  // Guardar las direcciones del contrato en un archivo JSON
+  const addresses = {
+    JamToken: jamToken.address,
+    StellartToken: stellartToken.address,
+    TokenFarm: tokenFarm.address,
+  };
+
+  const addressesPath = path.join(__dirname, '..', 'src', 'contracts', 'addresses.json');
+  fs.writeFileSync(addressesPath, JSON.stringify(addresses, null, 2));
+
+  console.log(`Contract addresses saved to ${addressesPath}`);
 }
 
 main()
