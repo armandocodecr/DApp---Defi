41cebfe61279b7bb30dc9d4d0b7bf2cc57f52450
ArmandoCode <totovip1464@gmail.com>
2024-06-07 20:42:18 -0600

    update implementation to connect the wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,145 @@
-import React, { useEffect, useState } from 'react';
-import { ethers } from 'ethers';
-import JamToken from '../artifacts/contracts/JamToken.sol/JamToken.json';
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import { useAccount } from "wagmi";
 
-const App: React.FC = () => {
-  const [account, setAccount] = useState<string | null>(null);
-  const [jamToken, setJamToken] = useState<ethers.Contract | null>(null);
-  const [stakeInput, setStakeInput] = useState<string>("0");
-  const [balance, setBalance] = useState<string | null>(null);
+import { useConnect } from "./hooks";
+import { useToken } from "./hooks/useToken";
 
-  const justNumbers = new RegExp('^[0-9,$]*$');
+export function App () {
+
+  const [ stakeInput, setStakeInput ] = useState<string>("");
+  const [ loading, setLoading ] = useState(true);
+
+  const { address, isConnected } = useAccount();
+  const { useEthersSigner } = useConnect()
+
+  const provider = useEthersSigner();
+  const { jamToken, tokenFarm, balances, initBalances } = useToken()
 
   const handleStateInput = (value: string) => {
+    const justNumbers = new RegExp("^[0-9,$]*$");
     if (justNumbers.test(value)) {
       setStakeInput(value);
     }
   };
 
-  useEffect(() => {
-    const init = async () => {
-      if (window.ethereum) {
-        // Solicitar acceso a la cuenta
-        await window.ethereum.request({ method: 'eth_requestAccounts' });
-
-        // Verificar que window.ethereum esté disponible
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        const network = await provider.getNetwork();
-
-        // Asegurarse de que estamos en la red correcta
-        if (network.chainId === 1337) {
-          const signer = provider.getSigner();
-
-          // Obtener la cuenta
-          const account = await signer.getAddress();
-          setAccount(account);
-
-          // Obtener la instancia del contrato
-          const jamToken = new ethers.Contract(
-            '0x0a88BDA3DE97195Ab5c2550EB3dE41E7AbEE8948', // Reemplaza con la dirección de tu contrato
-            JamToken.abi,
-            signer
-          );
-          setJamToken(jamToken);
-
-          // Obtener el balance
-          const balance = await jamToken.balanceOf(account);
-          setBalance(ethers.utils.formatEther(balance));
-        } else {
-          console.error("Conéctate a la red de Ganache (ID: 1337)");
-        }
-      } else {
-        console.error("MetaMask no está instalado");
+  const loadWeb3 = async () => {
+    if (window.ethereum) {
+      try {
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+      } catch (error) {
+        console.error("User denied account access");
       }
-    };
+    } else {
+      window.alert("¡Deberías considerar usar Metamask!");
+    }
+  };
 
-    init();
+  useEffect(() => {
+    loadWeb3();
   }, []);
 
+  useEffect(() => {
+    initBalances();
+    setLoading(false)
+  }, [isConnected, provider, address]);
+
+  const stakeTokens = async () => {
+    if (jamToken && tokenFarm && address) {
+      setLoading(true);
+      const amount = ethers.utils.parseUnits(stakeInput, "ether");
+      try {
+        const approveTx = await jamToken.approve(tokenFarm.address, amount);
+        await approveTx.wait(); // Wait for the approval transaction to be mined
+
+        const stakeTx = await tokenFarm.stakeTokens(amount);
+        await stakeTx.wait(); // Wait for the stake transaction to be mined
+
+        initBalances()
+        setStakeInput("")
+        setLoading(false);
+      } catch (error) {
+        console.error("Error staking tokens:", error);
+        setLoading(false);
+      }
+    }
+  };
+
+  const unstakeTokens = async () => {
+    if (tokenFarm && address) {
+      setLoading(true);
+      try {
+        const unstakeTx = await tokenFarm.unstakeTokens();
+        await unstakeTx.wait(); // Wait for the unstake transaction to be mined
+
+        initBalances()
+        setStakeInput("")
+        setLoading(false);
+      } catch (error) {
+        console.error("Error unstaking tokens:", error);
+        setLoading(false);
+      }
+    }
+  };
+
   return (
-    <section className='w-full h-screen flex flex-col gap-5 justify-center items-center'>
-      <h1 className='text-[#242422] tracking-wider text-5xl font-bold'>MAKE YOUR STAKING!</h1>
-      <div className="w-[20rem] h-[20rem] flex flex-col gap-2 justify-center items-center bg-[#FDFBF6] border border-[#242422] rounded-lg">
-        {balance && <p className='tracking-wider text-xl font-normal'>Balance: {balance} ACM</p>}
-        <input 
-          type="text" 
-          className='p-2 outline-none bg-transparent border border-[#242422] tracking-wider text-base font-normal rounded-md'
-          value={stakeInput}
-          onChange={(e) => handleStateInput(e.target.value)} 
-        />
-        <div className="flex justify-center gap-2 pt-3 items-center">
-          <button type="button" className='min-w-[6rem] py-2 tracking-wider bg-[#242422] text-[#FDFBF6] text-base font-normal rounded-md'>Stake!</button>
-          <button type="button" className='min-w-[6rem] py-2 tracking-wider bg-transparent border border-[#242422] text-[#242422] text-base font-normal rounded-md'>Unstake!</button>
+    <section className="w-full h-screen flex flex-col gap-5 justify-center items-center">
+      <h1 className="text-[#242422] tracking-wider text-5xl font-bold">
+        MAKE YOUR STAKING!
+      </h1>
+      {loading ? (
+        <p className="text-[#242422] tracking-wider text-xl font-bold flex flex-col justify-center items-center">
+          Cargando...
+        </p>
+      ) : (
+        <div className="w-[30rem] py-16 flex flex-col gap-2 justify-center items-center bg-[#FDFBF6] border border-[#242422] rounded-lg">
+          <div className="w-[20.5rem]">
+            <div className="w-full flex justify-between items-center mb-10 p-2 border border-[#242422] rounded-lg">
+              <p className="text-[#242422] tracking-wider text-xl font-bold flex flex-col justify-center items-center">
+                In staking
+                <span className="font-normal">{balances.stakingBalance}</span>
+              </p>
+
+              <p className="text-[#242422] tracking-wider text-xl font-bold flex flex-col justify-center items-center">
+                Reward
+                <span className="font-normal">
+                  {balances.stellartTokenBalance}
+                </span>
+              </p>
+            </div>
+            {balances.jamTokenBalance && (
+              <p className="w-full tracking-wider text-xl flex justify-end font-normal">
+                Balance: {balances.jamTokenBalance} JAM
+              </p>
+            )}
+            <input
+              type="text"
+              className="p-2 w-full outline-none bg-transparent border border-[#242422] tracking-wider text-base font-normal rounded-md"
+              value={stakeInput}
+              placeholder="0"
+              onChange={(e) => handleStateInput(e.target.value)}
+            />
+          </div>
+          <div className="flex justify-center gap-2 pt-3 items-center">
+            <button
+              type="button"
+              onClick={stakeTokens}
+              className="min-w-[10rem] py-2 tracking-wider bg-[#242422] text-[#FDFBF6] text-base font-normal rounded-md"
+            >
+              Stake!
+            </button>
+            <button
+              type="button"
+              onClick={unstakeTokens}
+              className="min-w-[10rem] py-2 tracking-wider bg-transparent border border-[#242422] text-[#242422] text-base font-normal rounded-md"
+            >
+              Unstake!
+            </button>
+          </div>
         </div>
-      </div>
+      )}
     </section>
   );
-}
+};
 
 export default App;
