656dd6b3d0f0b52be882db7fbe5826a96c748b3d
ArmandoCode <totovip1464@gmail.com>
2024-06-09 11:51:27 -0600

    update hardhat config

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,5 +1,11 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
+const fs = require('fs');
+const path = require('path');
+
+const privateKey = fs.readFileSync(path.resolve(__dirname, '.secret'), 'utf-8').trim();
 
 module.exports = {
   solidity: "0.8.4",
@@ -8,5 +14,13 @@ module.exports = {
       url: "http://127.0.0.1:7545",
       network_id: "*", // cualquier id de red
     },
+    sepolia: {
+      url: 'https://sepolia.infura.io/v3/273723e027b54226b92c0187d333f329',
+      network: "11155111",
+      accounts: [privateKey]
+    },
+    // etherscan: {
+    //   apiKey: process.env.ETHERSCAN_API_KEY // Para la verificación del contrato en Sepolia
+    // }
   },
 };
